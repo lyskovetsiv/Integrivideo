36d4f73d83664b6d0ab3af287f5bbb38c8be4d3e
PC_lyskovets <lyskovetsiv@gmail.com>
2020-07-10 19:41:26 +0300

    fixed openPage method in ChatPage and added String return statement in generateInviteLink method

--- src/test/java/pages/ChatPage.java
+++ src/test/java/pages/ChatPage.java
@@ -12,16 +12,19 @@ import java.io.IOException;
 
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.testng.Assert.assertEquals;
 
 public class ChatPage extends BasePage {
 
+    private static final String CHAT_PAGE_URL = "https://dev.integrivideo.com/demo/chat/new";
     private static final String CHAT_CSS = ".chat";
     private static final String INVITE_USERS_TO_CHAT_BUTTON_ID = "invite-users-to-chat";
 
     @Override
     public ChatPage openPage() {
+        open(CHAT_PAGE_URL);
         return this;
     }
 
@@ -31,16 +34,15 @@ public class ChatPage extends BasePage {
         return this;
     }
 
-    public ChatPage generateInviteLink() {
+    public String generateInviteLink() throws IOException, UnsupportedFlavorException {
         $(byId(INVITE_USERS_TO_CHAT_BUTTON_ID)).click();
-        return this;
-    }
-
-    public String validateInviteURL() throws IOException, UnsupportedFlavorException {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Clipboard clipboard = toolkit.getSystemClipboard();
-        String chatLink = (String) clipboard.getData(DataFlavor.stringFlavor);
-        assertEquals(url(), chatLink, "Incorrect chat link");
-        return chatLink;
+        return (String) clipboard.getData(DataFlavor.stringFlavor);
+    }
+
+    public ChatPage validateInviteURL(String link) {
+        assertEquals(url(), link, "Incorrect chat link");
+        return new ChatPage();
     }
 }
