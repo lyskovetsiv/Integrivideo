402557c6cbf1582356152ad34534bcdfc567405e
PC_lyskovets <lyskovetsiv@gmail.com>
2020-07-13 00:09:20 +0300

    fixed description locator in ProjectCreationPage

--- src/test/java/pages/ProjectCreationPage.java
+++ src/test/java/pages/ProjectCreationPage.java
@@ -13,7 +13,7 @@ public class ProjectCreationPage extends BasePage {
     private static final String PROJECT_CREATION_PAGE_URL = "https://dev.integrivideo.com/app/projects/new";
     private static final String CREATE_BUTTON_XPATH = "//*[contains(@id, 'project-form')]/descendant::*[contains(text(), 'Create')]";
     private static final String PROJECT_NAME_NAME = "name";
-    private static final String DESCRIPTION_NAME = "description";
+    private static final String DESCRIPTION_CSS = ".form-control[name=description]";
     private static final String DOMAIN_NAME = "domains[]";
 
     @Override
@@ -30,7 +30,7 @@ public class ProjectCreationPage extends BasePage {
 
     public Project createNewProject(Project project) {
         $(byName(PROJECT_NAME_NAME)).setValue(project.getProjectName());
-        $(byName(DESCRIPTION_NAME)).setValue(project.getDescription());
+        $(DESCRIPTION_CSS).setValue(project.getDescription());
         $(byName(DOMAIN_NAME)).setValue(project.getDomain());
         $(byXpath(CREATE_BUTTON_XPATH)).click();
         ProjectsPage projectsPage = new ProjectsPage();
