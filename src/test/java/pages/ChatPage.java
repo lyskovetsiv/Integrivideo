011ef4c25a9238edf06fc56e3d94e4db9519516c
PC_lyskovets <lyskovetsiv@gmail.com>
2020-07-21 15:01:02 +0300

    new chat tests

--- src/test/java/pages/ChatPage.java
+++ src/test/java/pages/ChatPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
@@ -12,6 +13,7 @@ import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 import static utils.TimeStamp.getTimeStamp;
 
 public class ChatPage extends BasePage {
@@ -22,7 +24,12 @@ public class ChatPage extends BasePage {
     private static final String ENTER_MESSAGE_CSS = "div.integri-chat-input>textarea";
     private static final String SEND_MESSAGE_BUTTON_CSS = ".integri-chat-send-message";
     private static final String ALL_MESSAGES_CSS = ".integri-chat-message-text";
+    private static final String ALL_EDITED_MESSAGES_CSS = ".integri-chat-message-edited .integri-chat-message ";
     private static final String MESSAGE_DATES_CSS = ".integri-chat-message-date";
+    private static final String EDIT_MESSAGES_BUTTON_CSS = ".integri-chat-edit-message";
+    private static final String ALL_MESSAGES_TO_EDIT_CSS = "div.integri-chat-message >textarea";
+    private static final String DELETE_MESSAGES_BUTTON_CSS = ".integri-chat-remove-message";
+    private static final String REMOVED_MESSAGE_CSS = ".integri-chat-message-utility .integri-chat-message-text";
 
     @Override
     public ChatPage openPage() {
@@ -58,6 +65,29 @@ public class ChatPage extends BasePage {
         String timeStamp = getTimeStamp();
         assertEquals($$(ALL_MESSAGES_CSS).last().getText(), message, "Message is incorrect");
         assertEquals($$(MESSAGE_DATES_CSS).last().getText(), timeStamp, "Date is incorrect");
+        return this;
+    }
+
+    public String editMessage(String editedMessage) {
+        $$(EDIT_MESSAGES_BUTTON_CSS).last().hover().click();
+        $$(ALL_MESSAGES_TO_EDIT_CSS).last().setValue(editedMessage).pressEnter();
+        return editedMessage;
+    }
+
+    public ChatPage verifyEditedMessage(String newMessage) {
+        assertEquals($$(ALL_EDITED_MESSAGES_CSS).last().getAttribute("innerText"), newMessage, "An edited message is incorrect");
+        return this;
+    }
+
+    public ChatPage deleteMessage() {
+        $$(DELETE_MESSAGES_BUTTON_CSS).last().hover().click();
+        Selenide.switchTo().alert().accept();
+        return this;
+    }
+
+    public ChatPage verifyDeletedMessage() {
+        assertTrue($$(REMOVED_MESSAGE_CSS).last().waitUntil(Condition.visible, 20000).isDisplayed(), "There's no remove notification");
+        assertEquals($$(REMOVED_MESSAGE_CSS).last().getText(), "removed...", "Removed message text is incorrect");
         return this;
     }
 }
--- src/test/java/tests/ChatTest.java
+++ src/test/java/tests/ChatTest.java
@@ -6,10 +6,25 @@ import org.testng.annotations.Test;
 public class ChatTest extends BaseTest{
 
     @SneakyThrows
-    @Test
-    public void messageValidation() {
+    @Test(priority = 1)
+    public void chatUrlValidation() {
         chatPage.openPage()
-                .validateInviteURL(chatPage.generateInviteLink())
-                .verifyMessage(chatPage.sendMessage("Test message 1234!@#$%^&*("));
+                .validateInviteURL(chatPage.generateInviteLink());
+    }
+
+    @Test(priority = 2)
+    public void messageValidation() {
+                chatPage.verifyMessage(chatPage.sendMessage("Test message 1234!@#$%^&*("));
+    }
+
+    @Test(priority = 3)
+    public void editMessageValidation() {
+        chatPage.verifyEditedMessage(chatPage.editMessage("Edited message"));
+    }
+
+    @Test(priority = 4)
+    public void deleteMessageValidation() {
+        chatPage.deleteMessage()
+                .verifyDeletedMessage();
     }
 }
